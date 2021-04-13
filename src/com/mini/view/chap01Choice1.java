da8f59bf80d7b75742316cbefbe5e0a10139a393
duswns <qpwlxk123@gmail.com>
2021-04-14 01:14:22 +0900

    chap01 choice01 호감도 수정입니다

--- src/com/mini/view/chap01Choice1.java
+++ src/com/mini/view/chap01Choice1.java
@@ -190,15 +190,15 @@ public class chap01Choice1 extends JFrame {
 				int stage2 = Integer.parseInt(num);
 				stage2++;
 				
-//				String lovePointnum = lovePointLabel.getText();
-//				int lovePoint = Integer.parseInt(lovePointnum);
-//				lovePoint += 10; //호감도 증가하는 값 일단 임의로 그냥 넣음 나중에 수정해야함
+				String lovePointnum = lovePointLabel.getText();
+				int lovePoint = Integer.parseInt(lovePointnum);
+				lovePoint += 10; //호감도 증가하는 값 일단 임의로 그냥 넣음 나중에 수정해야함
 				
-				int lvp = lovePoint + 2000;
+				
 				System.out.println("========================" + lovePoint+"==================================");
 				//미니게임 gui 호출
 				CardGame.startView(); //시작팝업
-				GameFrame game = new GameFrame(name, stage2, lvp);
+				GameFrame game = new GameFrame(name, stage2, lovePoint);
 			}
 
 		});
