cf9b82ee9f76e1da6120681d3e3f596036af318c
ahyun <ahyun@DESKTOP-S6R2HHK>
2021-04-13 18:12:31 +0900

    1번 문제 해결

--- src/com/mini/view/warning.java
+++ src/com/mini/view/warning.java
@@ -11,6 +11,20 @@ public class warning {
 	
 	public warning() {
 		
+		JLabel warning = new JLabel("프로그램을 종료하시겠습니까?");
+		warning.setFont(new Font("배달의민족 주아", Font.PLAIN, 15));
+		
+		int result = JOptionPane.showConfirmDialog(null,warning,"경고문",JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+		
+		if(result == JOptionPane.YES_OPTION) {
+			System.out.println("YES");
+			System.exit(0);
+		} else if(result == JOptionPane.NO_OPTION) {
+			System.out.println("NO");
+		} else {
+			
+		}
+		
 	}
 	
 	public warning(String name, int stage, int lovePoint) {
