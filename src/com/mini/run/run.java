4f21c7d31f5f9f84a27c901db409220837e00519
Hyon Woo Kang <87benzenes@gmail.com>
2021-04-15 01:41:12 +0900

    run 수정

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,14 +1,14 @@
 package com.mini.run;
 
-import com.mini.view.chap03Narration0;
+import com.mini.view.FristMain;
 
 public class run {
 	public static void main(String[] args) {
 		
 //		new FristMain();
-//		new FristMain();
+		new FristMain();
 		
-		new chap03Narration0("호동이", 1, 1);
+//		new chap03Narration0("호동이", 1, 1);
 //		new chap01Choice1("a",1,50);
 		
 //		new chap01Talk3("a",1,50);
