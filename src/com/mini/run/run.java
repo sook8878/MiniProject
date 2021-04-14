8e684561b172f4a180db42243633b42da67c2ff2
SORA <SORA@DESKTOP-0MLIQ7M>
2021-04-14 22:26:15 +0900

    아바라까지

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,13 +1,15 @@
 package com.mini.run;
 
-import com.mini.view.FristMain;
+import com.mini.view.chap01Talk3;
 
 public class run {
 	public static void main(String[] args) {
 		
-		new FristMain();
+		//new FristMain();
 		
 		//new chap01Narration9("a",1,50);
+		
+		new chap01Talk3("a",1,50);
 		
 	}
 }
