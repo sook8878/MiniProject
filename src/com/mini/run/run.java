c41c0c7a7563bf0affc8be3613e107c5279cdf21
duswns <qpwlxk123@gmail.com>
2021-04-14 23:28:24 +0900

    커밋

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,16 +1,16 @@
 package com.mini.run;
 
-import com.mini.view.chap01Talk3;
+import com.mini.view.FristMain;
 
 public class run {
 	public static void main(String[] args) {
 		
-		//new FristMain();
+		new FristMain();
 		
 //		new chap03Narration1("호동이", 1, 1);
 //		new chap01Choice1("a",1,50);
 		
-		new chap01Talk3("a",1,50);
+//		new chap01Narration6("a",1,50);
 		
 	}
 }
