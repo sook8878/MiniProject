9d4f36ce5666f4d257790c96aa31aa73cc79af3b
duswns <qpwlxk123@gmail.com>
2021-04-15 00:02:36 +0900

    챕터2넘어가는 이미지 수정

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,16 +1,16 @@
 package com.mini.run;
 
-import com.mini.view.FristMain;
+import com.mini.view.chap02Narration0;
 
 public class run {
 	public static void main(String[] args) {
 		
-		new FristMain();
+//		new FristMain();
 		
 //		new chap03Narration1("호동이", 1, 1);
 //		new chap01Choice1("a",1,50);
 		
-//		new chap01Narration6("a",1,50);
+		new chap02Narration0("a",1,50);
 		
 	}
 }
