7bc2033c8f4cf331536c02ddf177fd6c10e12e2b
tj2 <tj2@DESKTOP-64T8B5K>
2021-04-15 16:33:43 +0900

    이미지

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,11 +1,12 @@
 package com.mini.run;
 
-import com.mini.view.badEnd;
+import com.mini.view.chap03Narration0;
 
 public class run {
 	public static void main(String[] args) {
 		
 //		new FristMain();
-		new badEnd("a", 1, 0);
+//		new badEnd("a", 1, 0);
+		new chap03Narration0("a",1,50);
 	}
 }
