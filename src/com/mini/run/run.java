2d63111a10f27c044237f48cc87446a1775da5bd
연준 <qpwlxk123@gmail.com>
2021-04-15 18:09:11 +0900

    이미지 최종수정

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,16 +1,16 @@
 package com.mini.run;
 
-import com.mini.view.FristMain;
+import com.mini.view.chap04Talk4;
 
 public class run {
 	public static void main(String[] args) {
 		
-		new FristMain();
+//		new FristMain();
 //		new chap02Narration0("a", 1, 0);
 //		new badEnd("a", 1, 0);
 
 
-//		new chap04Talk2("a", 1, 0);
+		new chap04Talk4("a", 1, 0);
 
 	}
 }
--- src/com/mini/view/chap04Talk5.java
+++ src/com/mini/view/chap04Talk5.java
@@ -45,7 +45,7 @@ public class chap04Talk5 extends JFrame {
 		pan.setBackground(b);
 
 		// 상단에 사람 이미지
-		Image personImg = new ImageIcon("image/부장3.jpg").getImage();
+		Image personImg = new ImageIcon("image/고백씬.gif").getImage();
 		personImg.getScaledInstance(780, 300, Image.SCALE_SMOOTH);
 		JLabel person = new JLabel(new ImageIcon(personImg));
 
