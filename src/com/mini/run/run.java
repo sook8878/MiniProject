9d642745165aef33de289eefdd3e7ea3a61c58c9
연준 <qpwlxk123@gmail.com>
2021-04-15 16:17:09 +0900

    run

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,12 +1,12 @@
 package com.mini.run;
 
-import com.mini.view.chap04Talk5;
-import com.mini.view.happyEnd;
+import com.mini.view.FristMain;
+import com.mini.view.chap04Narration0;
 
 public class run {
 	public static void main(String[] args) {
 		
 //		new FristMain();
-		new chap04Talk5("a", 1, 0);
+		new chap04Narration0("a", 1, 0);
 	}
 }
