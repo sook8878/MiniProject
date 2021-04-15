f01c41c631cfe9b9d4587361bf8543d3c69e68e4
Hyon Woo Kang <87benzenes@gmail.com>
2021-04-15 11:06:25 +0900

    챕터03 이미지 수정

--- src/com/mini/run/run.java
+++ src/com/mini/run/run.java
@@ -1,14 +1,14 @@
 package com.mini.run;
 
-import com.mini.view.chap02Talk2;
+import com.mini.view.FristMain;
 
 public class run {
 	public static void main(String[] args) {
 		
-//		new FristMain();
+		new FristMain();
 //		new chap03Narration1("호동이", 1, 1);
 //		new chap01Choice1("a",1,50);
-		new chap02Talk2("a",1,50);
+//		new chap02Talk2("a",1,50);
 
 
 	}
